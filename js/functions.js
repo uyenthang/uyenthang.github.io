491da97ddd8ff0cf0a7a05815bb3af6a21889476
Thang Nguyen <thangnh@Thangs-MacBook-Pro.local>
2016-04-24 13:53:53 +0900

    update js

--- js/functions.js
+++ js/functions.js
@@ -93,25 +93,18 @@ function startHeartAnimation() {
 })(jQuery);
 
 function timeElapse(date){
-	var current = Date();
-	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
-	var days = Math.floor(seconds / (3600 * 24));
-	seconds = seconds % (3600 * 24);
-	var hours = Math.floor(seconds / 3600);
-	if (hours < 10) {
-		hours = "0" + hours;
-	}
-	seconds = seconds % 3600;
-	var minutes = Math.floor(seconds / 60);
-	if (minutes < 10) {
-		minutes = "0" + minutes;
-	}
-	seconds = seconds % 60;
-	if (seconds < 10) {
-		seconds = "0" + seconds;
-	}
-	var result = "<span class=\"digit\">" + days + "</span> days <span class=\"digit\">" + hours + "</span> hours <span class=\"digit\">" + minutes + "</span> minutes <span class=\"digit\">" + seconds + "</span> seconds"; 
+	var current = new Date();
+	var h = current.getHours();
+	var m = current.getMinutes();
+	var s = current.getSeconds();
+	m = checkTime(m);
+	s = checkTime(s);
+	var result = "<span class=\"digit\"> Tues, 26 Apr 2016 " +h+ ":" +m+ ":" +s+ "</span>";  
 	$("#elapseClock").html(result);
+}
+function checkTime(i){
+  if (i < 10 ) { i = "0" + i };
+  return i;
 }
 
 function showMessages() {
